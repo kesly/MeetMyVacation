5e889c9283516ee613c1fcf1253233dcd31aa7d9
kesly gassant <kellorkeskes@hotmail.fr>
2019-03-12 18:06:51 +0100

    first commit

---
+++ client2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+    // recuperer données
+
+    //$ville=$_GET[""];
+
+    // configurer proxy
+
+    // créer url
+
+
+     ?>
+
+
+  </body>
+</html>
